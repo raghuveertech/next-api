eea625511f114507fe2b383a3b0fd525d8e24f51
Raghuveer <raghuveerp25@gmail.com>
2025-10-17 16:47:49 +0530

    users list and users post complete

--- src/app/users/[userId]/page.jsx
+++ src/app/users/[userId]/page.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import './user-details.css';
+import Link from 'next/link';
+import apiURLs from '@/app/api/urls';
 
-const UserDetails = () => {
+const UserDetails = async (props) => {
+  let params = await props.params;
+  let userId = params.userId;
+
+  let usersResponse = await fetch(`${apiURLs.getUsers}/${userId}`);
+  let userInfo = await usersResponse.json();
+
+  let postsResponse = await fetch(apiURLs.getPosts);
+  let postsList = await postsResponse.json();
+
+  let userPosts = postsList.filter((post) => {
+    return Number(post.userId) === Number(userId);
+  });
+
   return (
     <div className='container'>
-      <h1>User Details</h1>
+      <h1>
+        <Link href={'/'}> Home </Link> / <Link href={'/users'}> Users </Link> /
+        User Details
+      </h1>
+      <div className='user-details'>
+        <h2>{userInfo.name}</h2>
+        <p>Email: {userInfo.email}</p>
+        <p>
+          Address: {userInfo.address.street}, {userInfo.address.city},{' '}
+          {userInfo.address.zipcode}
+        </p>
+      </div>
+      <div className='posts-list'>
+        {userPosts.map((post) => {
+          return (
+            <div className='item' key={post.id}>
+              <h3>{post.title}</h3>
+              <p>
+                <Link className='learn-more' href={`/posts/${post.id}`}>
+                  Learn More
+                </Link>
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import './users.css';
+import apiURLs from '../api/urls';
+import Link from 'next/link';
 
-const Users = () => {
+const Users = async () => {
+  let response = await fetch(apiURLs.getUsers);
+
+  let users = await response.json();
+
   return (
-    <div>
-      <h1>Users</h1>
+    <div className='container'>
+      <h1>
+        <Link href={'/'}>Home</Link> / Users List
+      </h1>
+      <div className='users-list'>
+        {users.map((user) => {
+          return (
+            <div className='item' key={user.id}>
+              <h3>{user.name}</h3>
+              <p>{user.company.name}</p>
+              <p>
+                <Link className='view-user' href={`/users/${user.id}`}>
+                  View User
+                </Link>
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
